761a220e4e99c7fd5e8ab2fff5b42b83b3326f33
Liuxh000 <107558512+Liuxh000@users.noreply.github.com>
2026-01-07 16:10:42 +0800

    Update dataset_loader.py

--- datasets/dataset_loader.py
+++ datasets/dataset_loader.py
@@ -4,48 +4,72 @@ from typing import List
 from PIL import Image
 import numpy as np
 from torchvision import transforms
-Image.MAX_IMAGE_PIXELS = None
-class Datasets(FewShotDataset):
-     def __init__(self, args, transform, split='train'):
-         data_dir = os.path.join(args.dataset_dir, split)
-         images_path = []
-         labels = []
-         self.class_names = []
-         if transform is None:
-             self.transforms = transforms.Compose([
-                 transforms.Resize(256),
-                 transforms.RandomResizedCrop(240),
-                 transforms.CenterCrop(224),
-                 transforms.RandomHorizontalFlip(),
-                 transforms.ToTensor(),
-                transforms.Normalize(np.array([x / 255.0 for x in [125.3, 123.0, 113.9]]),np.array([x / 255.0 for x in [63.0, 62.1, 66.7]]))])
-         else:
-             self.transform = transform
 
-         if os.path.isdir(data_dir):
-            for idx, label in enumerate(os.listdir(data_dir)):
+Image.MAX_IMAGE_PIXELS = None
+
+
+class Datasets(FewShotDataset):
+    def __init__(self, args, transform=None, split='train'):
+        data_dir = os.path.join(args.dataset_dir, split)
+
+        images_path = []
+        labels = []
+        self.class_names = []
+
+        image_size = args.image_size  
+
+        if transform is None:
+            if split == 'train':
+                self.transforms = transforms.Compose([
+                    transforms.Resize(int(image_size * 1.1)),   
+                    transforms.RandomResizedCrop(
+                        image_size,
+                        scale=(0.7, 1.0),
+                        ratio=(0.75, 1.33)
+                    ),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=np.array([125.3, 123.0, 113.9]) / 255.0,
+                        std=np.array([63.0, 62.1, 66.7]) / 255.0
+                    ),
+                ])
+            else:  # val / test
+                self.transforms = transforms.Compose([
+                    transforms.Resize(int(image_size * 1.1)),
+                    transforms.CenterCrop(image_size),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=np.array([125.3, 123.0, 113.9]) / 255.0,
+                        std=np.array([63.0, 62.1, 66.7]) / 255.0
+                    ),
+                ])
+        else:
+            self.transforms = transform
+
+        if os.path.isdir(data_dir):
+            for idx, label in enumerate(sorted(os.listdir(data_dir))):
                 self.class_names.append(label)
                 label_dir = os.path.join(data_dir, label)
                 for image_path in os.listdir(label_dir):
                     images_path.append(os.path.join(label_dir, image_path))
                     labels.append(idx)
-         else:
-             raise ValueError('wrong dataset structure')
-         self.class_to_label = {v: k for k, v in enumerate(self.class_names)}
-         self.images_path = images_path
-         self.labels = labels
+        else:
+            raise ValueError('Wrong dataset structure')
 
-     def __getitem__(self, item):
-         path, label = self.images_path[item], self.labels[item]
-         image = self.transforms(Image.open(path).convert('RGB'))
-         return image, label
+        self.class_to_label = {v: k for k, v in enumerate(self.class_names)}
+        self.images_path = images_path
+        self.labels = labels
 
-     def __len__(self):
-         return len(self.images_path)
+    def __getitem__(self, item):
+        path, label = self.images_path[item], self.labels[item]
+        image = self.transforms(Image.open(path).convert('RGB'))
+        return image, label
 
-     def get_labels(self) -> List[int]:
-         return self.labels
+    def __len__(self):
+        return len(self.images_path)
 
-
+    def get_labels(self) -> List[int]:
+        return self.labels
 
 
